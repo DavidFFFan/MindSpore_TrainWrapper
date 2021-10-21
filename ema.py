[fixed] inhert train_one_step from mindspore.nn.TrainOneStepWithLossScaleCell, add clip global norm and gradients ema, transfer ops to cpu to save npu/gpu memory

ema.py
@@ -7,13 +7,20 @@ from mindspore.ops import functional as F
 from mindspore.ops import operations as P
 
 _ema_op = C.MultitypeFuncGraph("grad_ema_op")
+Assign = P.Assign()
+Assign.add_prim_attr("primitive_target", "CPU")
+AssignAdd = P.AssignAdd()
+AssignAdd.add_prim_attr("primitive_target", "CPU")
 
 
 @_ema_op.register("Tensor", "Tensor", "Tensor")
 def _ema_weights(factor, ema_weight, weight):
     """Apply grad sum to cumulative gradient."""
-    add = P.Assign()
-    return add(ema_weight, ema_weight * factor + weight * (1 - factor))
+    """ema_weight = ema_weight * factor + weight * (1-factor), factor is always 0.9999"""
+    Assign(ema_weight, ema_weight * factor)
+    Assign(weight, weight * (1 - factor))
+
+    return AssignAdd(ema_weight, weight)
 
 
 class EMACell(nn.Cell):